bdbea2f6e47e920995fe8150d7589ae6b6526b26
Dheeraj Bhateja <dheerajbhateja@gmail.com>
2020-03-30 19:47:14 +0530

    Created a project

---
+++ GitProj1/GitProj1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitProj1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
